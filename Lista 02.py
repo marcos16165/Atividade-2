e4631ec15adc76dd01798a35644d925b6495c54e
marcos16165 <38138237+marcos16165@users.noreply.github.com>
2018-07-18 13:19:59 -0300

    Update Lista 02.py

--- Lista 02.py
+++ Lista 02.py
@@ -2,7 +2,7 @@ def questao1():
     return 5**2, 9*5, 15/12, 12/15, 15//12, 12//15, 5%2, 9%5, 15%12, 12%15, 6%6, 0%7
 
 def questao2():
-    return print('5 horas')
+    return ('5 horas')
 
 def  questao3():
     print('Entre com a hora atual: ')
@@ -10,7 +10,7 @@ def  questao3():
     print('Quanto tempo deseja esperar: ')
     e = int(input())
     resto = (h + e) % 24
-    return print('O depertador tocará ás', resto,'horas.')
+    return ('O depertador tocará ás', resto,'horas.')
 
 def questao5():
     p1 = str('só')
@@ -33,14 +33,14 @@ def questao7():
     print('Entre com a quantidade de anos:')
     t = int(input())
     a = 10000 * (1 + (0.08/12))**(12*t)
-    return print('O valor do jutos depois de', t,'é ', a,'.')
+    return ('O valor do jutos depois de', t,'é ', a,'.')
 
 
 def questao8():
     print('Entre com o raio')
     raio = float(input())
     pi = 2 * 3.1415 * raio
-    return print ('A área do círculo é: ', pi)
+    return ('A área do círculo é: ', pi)
 
 
 def questao9():
@@ -49,7 +49,7 @@ def questao9():
     print('Entre com a largura do retângulo')
     la = float(input())
     ar = al * la
-    return print('A área do retângulo é:', ar)
+    return ('A área do retângulo é:', ar)
 
 
 def questao10():
@@ -58,18 +58,18 @@ def questao10():
     print('Entre com a quantidade de litros de gasolina consumidos: ')
     l = float(input())
     gasto = km / l 
-    return print('O carro consume ', gasto, 'litros por km.')
+    return ('O carro consume ', gasto, 'litros por km.')
 
 
 def questao11():
     print('Entre com a temperatura em C')
     ce =int(input())
-    return print(' A temperatura em fahrenheit é', ((ce * 9) / 5) + 32)
+    return (' A temperatura em fahrenheit é', ((ce * 9) / 5) + 32)
 
 
 def questao12():
     print('Entre com a temperatura em F')
     fa=int(input())
-    return print ('A temperatura em Celsius é', ((fa - 32) * 5) / 9)
+    return ('A temperatura em Celsius é', ((fa - 32) * 5) / 9)
 
         
